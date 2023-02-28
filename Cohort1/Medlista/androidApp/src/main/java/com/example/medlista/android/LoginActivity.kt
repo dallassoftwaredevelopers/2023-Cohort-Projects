899b468b045634797f452c5394b5e5827b42d076
cmkarako <33211829+cmkarako@users.noreply.github.com>
2023-02-28 16:41:14 -0600

    Working on login UI

--- Cohort1/Medlista/androidApp/src/main/java/com/example/medlista/android/LoginActivity.kt
+++ Cohort1/Medlista/androidApp/src/main/java/com/example/medlista/android/LoginActivity.kt
@@ -1,15 +1,37 @@
 package com.example.medlista.android
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CutCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.OutlinedTextField
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.blur
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.medlista.android.ui.theme.MedlistaTheme
 
 class LoginActivity : ComponentActivity() {
@@ -22,12 +44,99 @@ class LoginActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    Login()
                 }
             }
         }
     }
 }
+
+
+@Composable
+fun Login() {
+    Box(modifier = Modifier.fillMaxSize()) {
+        androidx.compose.foundation.Image(
+            painter = painterResource(id = R.drawable.pills),
+            contentDescription = "Image of medication pills.",
+            modifier = Modifier
+                .fillMaxSize()
+                .blur(4.dp),
+            contentScale = ContentScale.Crop
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(28.dp)
+                .alpha(0.5f)
+                .clip(
+                    CutCornerShape(
+                        topStart = 8.dp,
+                        topEnd = 16.dp,
+                        bottomStart = 16.dp,
+                        bottomEnd = 8.dp
+                    )
+                )
+                .background(MaterialTheme.colors.background)
+        )
+
+        Column(
+            Modifier
+                .fillMaxSize()
+                .padding(48.dp),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LoginHeader()
+            LoginFields()
+            LoginFooter()
+        }
+    }
+}
+
+@Composable
+fun LoginHeader() {
+    Text(text = "Welcome Back", fontSize = 36.sp, fontWeight = FontWeight.ExtraBold)
+    Text(text = "Sign in to continue", fontSize = 18.sp, fontWeight = FontWeight.SemiBold)
+}
+
+@Composable
+fun LoginFields() {
+
+}
+
+@Composable
+fun LoginFooter() {
+
+}
+
+// making a reusable component so we can use it for any field
+@Composable
+fun InputField(
+    value: String,
+    label: String,
+    placeholder: String,
+    visualTransformation: VisualTransformation = VisualTransformation.None,
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+    leadingIcon : @Composable (() -> Unit)? = null,
+    trailingIcon: @Composable (() -> Unit)? = null,
+    onValueChange: (String) -> Unit
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        label = {
+            Text(text = label)
+        },
+        placeholder = {
+            Text(text = placeholder)
+        },
+        visualTransformation = visualTransformation,
+        keyboardOptions = keyboardOptions,
+        leadingIcon = leadingIcon,
+        trailingIcon = trailingIcon
+        )
+}
+
 
 @Composable
 fun Greeting(name: String) {
@@ -38,6 +147,6 @@ fun Greeting(name: String) {
 @Composable
 fun DefaultPreview2() {
     MedlistaTheme {
-        Greeting("Android")
+        Login()
     }
 }
