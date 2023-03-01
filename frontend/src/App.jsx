b743ad4e037ec67a2605f982c5705fce6c614f90
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2023-02-28 21:40:46 -0600

    ADD home and 404 routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,12 +10,6 @@ function App() {
     <div className="App">
      <Routes>
       <Route path="/" element={<h1>Home</h1>} />
-      <Route path="/login" element={<h1>login</h1>} />
-      <Route path="/signup" element={<h1>signup</h1>} />
-      <Route path="/search" element={<h1>Search</h1>} />
-      <Route path="/add" element={<h1>Add recipe</h1>} />
-      <Route path="/recipe/:recipeId" element={<h1>recipe page</h1>} />
-      <Route path="/dashboard" element={<h1>dashboard</h1>} />
       <Route path="/*" element={<h1>404 no page</h1>} />
 
       
