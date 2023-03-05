b3607e4a3675f2aeef2a0118d5c7f58996c9ecb5
walter <walterscottwilliams@hotmail.com>
2023-03-04 21:38:02 -0600

    add createUser and login route and controller

--- backend/routes.js
+++ backend/routes.js
@@ -1,16 +1,26 @@
 const express = require('express');
-
 const router = express.Router();
+const User = require('./models/userModel.js');
+const { createUser,login } = require('./controllers/userController.js');
+const bcrypt = require('bcrypt');
+const saltRounds = 12;
 
-// sample route
-// a  route like this would fire
-router.get('/', (req, res) => {
-    try {
-        res.json({msg: 'test GET request working'});
-    } catch(error) {
-        res.status(400).json({error: 'something went wrong'});
-    }
+
+router.post('/newUser', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    const encryptedPassword = await bcrypt.hash(password, saltRounds);
+    const newUser = new User({ username, email, password: encryptedPassword });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 });
+
+router.post('/login', login);
+
+module.exports = router;
 
 /** SAMPLE ROUTES
  * a route like this would fire when going to /api/recipes because /api/ is defined as the route precursor in server.js 
@@ -26,4 +36,6 @@ router.get('/', (req, res) => {
  * })
  */
 
-module.exports = router;
+
+
+
