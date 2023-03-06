0fe69d0945521443aedeef4c5442f57c56682577
Damian Padilla <damipad4@gmail.com>
2023-03-06 01:26:26 -0600

    feat: created recipe search page with search bar and wired up to nav bar (DSD100-30)

---
+++ frontend/src/pages/RecipeSearch/RecipeSearch.styles.js
@@ -0,0 +1,42 @@
+import styled from "styled-components";
+
+export const StyledRecipeSearch = styled.section`
+    background-color: white;
+    color: #242424;
+    height: 100vh;
+    & .title {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    & .search {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        @media (max-width: 768px){
+            flex-direction: column;
+        }
+    }
+    & input {
+        background-color: white;
+        color: #242424;
+        border-radius: 2em;
+        border: 1px solid #242424;
+        padding: 1em;
+        width: 30%;
+        @media (max-width: 768px){
+            width: 50%;
+        }
+    }
+    & .filter{
+        border: 1px solid #242424;
+        width: 15%
+    }
+    & .searchResults {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+
+`;
+
