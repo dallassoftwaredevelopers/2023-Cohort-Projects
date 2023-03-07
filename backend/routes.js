976baa7aec24496bc0b2eac05d633b0e0ae036ee
Jesus Mendoza <101015821+jesusdoza@users.noreply.github.com>
2023-03-07 09:18:45 -0600

    feat: add searchbyingredient endpoint

--- backend/routes.js
+++ backend/routes.js
@@ -1,25 +1,47 @@
-const express = require('express');
-
+const express = require("express");
+const API = require("./API");
 const router = express.Router();
 
 // sample route
 // a  route like this would fire
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
     try {
-        res.json({msg: 'test GET request working'});
-    } catch(error) {
-        res.status(400).json({error: 'something went wrong'});
+        res.json({ msg: "test GET request working" });
+    } catch (error) {
+        res.status(400).json({ error: "something went wrong" });
+    }
+});
+
+router.get("/searchbyingredient", async (req, res) => {
+    const ingredientsList = req.body.ingredients;
+
+    //verify ingredients exist
+    if (!ingredientsList) {
+        res.status(400).json({ error: "no ingredients" });
+        return;
+    }
+
+    //get data from api
+    try {
+        const response = await API.searchRecipeByIngredients(ingredientsList);
+        const recipes = await response.json();
+        res.status(200).json(recipes);
+    } catch (error) {
+        res.status(400).json({
+            message: "recipe search by ingredient error",
+            err: error,
+        });
     }
 });
 
 /** SAMPLE ROUTES
- * a route like this would fire when going to /api/recipes because /api/ is defined as the route precursor in server.js 
- * 
+ * a route like this would fire when going to /api/recipes because /api/ is defined as the route precursor in server.js
+ *
  * GET all recipes
  * router.get('/', (res, req) => {
  *      res.json();
  * });
- * 
+ *
  * GET one recipe
  * router.get('/:id', (res, req) => {
  *      res.json();
