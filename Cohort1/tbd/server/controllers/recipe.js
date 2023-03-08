bb1e2432838801ea2955fee02dd23de8fd41a803
Christina Takara <christina.takara@gmail.com>
2023-03-08 10:34:08 -0600

    updated server and controllers/recipe to start creating GET endpoint

--- Cohort1/tbd/server/controllers/recipe.js
+++ Cohort1/tbd/server/controllers/recipe.js
@@ -1,2 +1,21 @@
 const express = require('express');
-const Recipe = require('../models/recipes')
+const Recipe = require('../models/recipes');
+
+const router = express.Router();
+
+//Index (GET all)
+router.get("/", async (req, res) => {
+    try {
+        const recipes = await Recipe.find({});
+        console.log('search terms are: ' + req.query.q);
+        console.log(typeof(req.query.q))
+        res.json(recipes);
+
+    } catch (err) {
+        res.json({ err });
+    }
+})
+
+///api/recipes?q=ingredients[ingredient 1, ingredient 2]
+
+module.exports = router;
--- Cohort1/tbd/server/serverWorking.js
+++ Cohort1/tbd/server/serverWorking.js
@@ -7,6 +7,17 @@ const app = express();
 const recipesController = require("./controllers/recipe")
 const mongoose = require('mongoose');
 
+// useNewUrlParser: true,
+// useUnifiedTopology: true
+// fix deprecation warnings
+  mongoose.connect(process.env.MONGO_URI,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  
+  mongoose.connection.once('open', ()=> {
+    console.log('connected to mongo')
+  })
 
   app.use("/recipes", recipesController);
 
