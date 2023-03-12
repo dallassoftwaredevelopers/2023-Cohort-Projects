f5ef0c69b0686c8d5def860a1475dab690b65f21
Mike B <bowman.michael@gmail.com>
2023-03-11 22:29:33 -0500

    feat: Adding react-router-dom and creating initial routes / pages

--- Cohort1/tbd/client/tbd/src/App.js
+++ Cohort1/tbd/client/tbd/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import SearchPage from './search/SearchPage';
+import RecipePage from './recipe/RecipePage';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+			<h1>TBD Recipe App</h1>
+			<Routes>
+				<Route path="/" element={<SearchPage/>} />
+				<Route path="/recipe/:id" element={<RecipePage />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
     </div>
   );
+}
+
+function NotFound() {
+	return (
+		<>
+			<h1>Page not found</h1>
+			<p>That page doesn't exist yet.</p>
+		</>
+	);
 }
 
 export default App;
---
+++ Cohort1/tbd/client/tbd/src/search/SearchPage.js
@@ -0,0 +1,7 @@
+export default function SearchPage() {
+	return (
+		<div>
+			Search Page <br/> TODO Build this page
+		</div>
+	);
+}
