92a5f18b0bb84be8e4ad57146585e45092f8f3a2
Eric Aguayo <computergeek90@protonmail.com>
2023-03-13 14:26:18 -0500

    TBD-13 updating search.js page with new code

--- Cohort1/tbd/client/tbd/src/App.js
+++ Cohort1/tbd/client/tbd/src/App.js
@@ -1,28 +1,19 @@
-import logo from './logo.svg';
+import './index.css'; 
 import './App.css';
-
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1>Recipe</h1>
-        <p>
-          You can make 764 Recipes
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Recipe
-        </a>
-
-           <body>
+            <div id="search-bg">
+                  <h1>Recipe</h1>
+                 <p>
+                    You can make 764 Recipes
+                </p>
              <input type="text" placeholder="Find..."></input>
-       </body>
+            </div>
+                <div id="root"></div>
+       
       </header>
     </div>
   );
