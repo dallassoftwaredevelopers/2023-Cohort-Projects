3bd491603b1ed167aaee7bf6f3225c4e1e0f6b2b
Eric Aguayo <computergeek90@protonmail.com>
2023-03-13 19:01:49 -0500

    TBD-13 updates for rebase

---
+++ Cohort1/tbd/client/tbd/src/search/SearchPage.js
@@ -0,0 +1,13 @@
+import 'search.css'
+
+export default function SearchPage() {
+	return (
+		 <div id="search-bg">
+                    <h1>Super Cook</h1>
+                 <p>
+                    You can make 764 Recipes
+                </p>
+             <input type="text" placeholder="Recipe Search..."></input>
+            </div>
+	);
+}
