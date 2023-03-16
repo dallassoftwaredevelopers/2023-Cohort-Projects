1e41ac68d0f6391fc2d2a23b3b42e415501e04a1
RicardoCruz21 <abstergo21@gmail.com>
2023-03-16 18:59:27 -0500

    add configuration annotation

--- Cohort1/cohortProject/src/main/java/com/cohortE/cohortProject/config/SecurityConfig.java
+++ Cohort1/cohortProject/src/main/java/com/cohortE/cohortProject/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package com.cohortE.cohortProject.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+@Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
