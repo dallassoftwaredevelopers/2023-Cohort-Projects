b7fe00db77919ff118577bfe9848c67eaa655b64
RicardoCruz21 <abstergo21@gmail.com>
2023-03-16 19:24:44 -0500

    rename successful login url

--- Cohort1/cohortProject/src/main/java/com/cohortE/cohortProject/config/SecurityConfig.java
+++ Cohort1/cohortProject/src/main/java/com/cohortE/cohortProject/config/SecurityConfig.java
@@ -25,7 +25,7 @@ public class SecurityConfig {
                 .and()
                 .formLogin(form -> form
                         .loginPage("/login")
-                        .defaultSuccessUrl("/medication-list")
+                        .defaultSuccessUrl("/medications")
                         .failureUrl("/login?error=true")
                         .usernameParameter("email")
                         .passwordParameter("password")
