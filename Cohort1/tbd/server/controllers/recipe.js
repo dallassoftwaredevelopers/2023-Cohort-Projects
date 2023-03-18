cdf373f8fc6b51e958bc5a9fd1f62871242b8b5f
Mike B <allaboutmike@users.noreply.github.com>
2023-03-18 16:14:25 -0400

    chore: TBD-21 Adding eslint and configuring with AirBnB standards (#17)

--- Cohort1/tbd/server/controllers/recipe.js
+++ Cohort1/tbd/server/controllers/recipe.js
@@ -1,39 +1,36 @@
 const express = require('express');
-const Recipe = require('../models/recipes');
 const { URLSearchParams } = require('url');
+const Recipe = require('../models/recipes');
 
 const router = express.Router();
 
-//Index (GET all)
-router.get("/", async (req, res) => {
-    let url = new URL(req.protocol + '://' + req.hostname + req.originalUrl);
-    let params = new URLSearchParams(url.search)
-    let searchIngredients = params.getAll('ingredient');
+// Index (GET all)
+router.get('/', async (req, res) => {
+  const url = new URL(`${req.protocol}://${req.hostname}${req.originalUrl}`);
+  const params = new URLSearchParams(url.search);
+  const searchIngredients = params.getAll('ingredient');
 
-    if (searchIngredients.length === 0) {
-        try {
-            const recipes = await Recipe.find({});
-            res.json(recipes);
-
-        } catch (err) {
-            res.json({ err });
-        }
+  if (searchIngredients.length === 0) {
+    try {
+      const recipes = await Recipe.find({});
+      res.json(recipes);
+    } catch (err) {
+      res.json({ err });
     }
-    else {
-        // for multiple ingredients, iterate through array of searchIngredients
-        // after try/catch, if there are recipes, compare to saved recipes and only save the ones in both
-        // for partial matching, a more complex data structure would be needed
-        try {
-            const recipes = await Recipe.find({
-                "ingredientsList.ingredientName": searchIngredients[0]
-            });
-            res.json(recipes);
-
-        } catch (err) {
-            res.json({ err });
-        }
+  } else {
+    // for multiple ingredients, iterate through array of searchIngredients
+    // after try/catch, if there are recipes, compare to saved recipes and
+    // only save the ones in both for partial matching, a more complex data
+    // structure would be needed
+    try {
+      const recipes = await Recipe.find({
+        'ingredientsList.ingredientName': searchIngredients[0],
+      });
+      res.json(recipes);
+    } catch (err) {
+      res.json({ err });
     }
-    })
-
+  }
+});
 
 module.exports = router;
