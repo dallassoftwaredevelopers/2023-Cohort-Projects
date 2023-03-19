218143dca74deac5c42aa7fcf5d63a5872551a38
smith494 <court14104@gmail.com>
2023-03-19 13:26:57 -0500

    solving merge conflict

---
+++ Cohort1/cohortProject/src/main/java/com/cohortE/cohortProject/service/impl/MedicationServiceImpl.java
@@ -0,0 +1,33 @@
+package com.cohortE.cohortProject.service.impl;
+
+import com.cohortE.cohortProject.entity.Medication;
+import com.cohortE.cohortProject.entity.User;
+import com.cohortE.cohortProject.repository.MedicationRepository;
+import com.cohortE.cohortProject.service.MedicationService;
+import com.cohortE.cohortProject.service.UserService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MedicationServiceImpl implements MedicationService {
+
+    private final MedicationRepository medicationRepository;
+    private final UserService userService;
+
+    public MedicationServiceImpl(MedicationRepository medicationRepository, UserService userService) {
+        this.medicationRepository = medicationRepository;
+        this.userService = userService;
+    }
+
+    @Override
+    public List<Medication> getAllUserMedications() {
+        User user = userService.getCurrentUser();
+        return medicationRepository.findByUserId(user.getId());
+    }
+
+    @Override
+    public void deleteMedicationById(Long id) {
+        medicationRepository.deleteById(id);
+    }
+}
