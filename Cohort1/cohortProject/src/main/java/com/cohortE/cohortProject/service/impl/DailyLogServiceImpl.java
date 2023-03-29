6397601816b0881006c0acd33c0a2ddf78b21dce
smith494 <court14104@gmail.com>
2023-03-29 18:59:55 -0500

    Updated the cron job with the correct @

--- Cohort1/cohortProject/src/main/java/com/cohortE/cohortProject/service/impl/DailyLogServiceImpl.java
+++ Cohort1/cohortProject/src/main/java/com/cohortE/cohortProject/service/impl/DailyLogServiceImpl.java
@@ -22,7 +22,7 @@ public class DailyLogServiceImpl implements DailyLogService {
         this.reminderService = reminderService;
         this.medicationLogService = medicationLogService;
     }
-    @Scheduled(cron = "0 0 0 * * *") //runs every 10 seconds "*/10 * * * * *"//  this code is for runs every day at midnight  "0 0 0 * * *"
+    @Scheduled(cron = "@midnight") //  this code is for runs every day at midnight  "0 0 0 * * *"
     @Override
     public void addNewDailyMedicationLog() {
         List<Reminder> listOfDailyReminders  =  reminderService.getAllDailyReminders();
