f85193aa9cc3a26edaa750083cf815c70e5567bc
Eric Aguayo <computergeek90@protonmail.com>
2023-04-03 15:48:02 -0500

    finished changes for api

--- Cohort1/tbd/client/tbd/src/search/SearchPage.js
+++ Cohort1/tbd/client/tbd/src/search/SearchPage.js
@@ -1,13 +1,41 @@
 import './search.css';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 export default function SearchPage() {
-	return (
-		<div id="search-bg">
-			<h1>Super Cook</h1>
-			<p>
-				You can make 764 Recipes
-			</p>
-			<input type="text" placeholder="Recipe search..."></input>
-		</div>
-	);
+  const [searchQuery, setSearchQuery] = useState('');
+   const api = axios.create({
+    baseURL: 'http://localhost:3001/api/',
+    
+  });
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    
+    search(searchQuery);
+  }
+
+  function search(query) {
+    api.get(`recipes?ingredient=${query}`)
+      .then(response => {
+        
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.error(error);
+      });
+  }
+
+ return (
+    <div id="search-bg">
+      <h1>Super Cook</h1>
+      <p>You can make 764 Recipes</p>
+      <form action="GET" onSubmit={handleSubmit}>
+        <input type="text" placeholder="Recipe search..." onChange={(e) => setSearchQuery(e.target.value)} />
+        <button type="submit" className="btn-recipe-search">
+          Search <i className="fa fa-search"></i>
+        </button>
+      </form>
+    </div>
+  );
 }
