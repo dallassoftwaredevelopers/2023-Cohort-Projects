bb2c1048a4a68e1be7441d16eeda00cdd7140f7a
cmtakara <93676739+cmtakara@users.noreply.github.com>
2023-04-03 11:59:41 -0500

    updates for recipe page, including helmet for cors (#19)
    * updates for recipe page, including helmet for cors
    
    * updated useEffect dependencies
    
    * commented out setting id until it can be used
    
    * commented out useparams until it can be used

--- Cohort1/tbd/server/server.js
+++ Cohort1/tbd/server/server.js
@@ -1,12 +1,17 @@
 require('dotenv').config();
 const express = require('express');
+const helmet = require('helmet');
+const cors = require('cors');
 // use the port from .env, if it is there
 const PORT = process.env.PORT || 3001;
 // create the express app
 const app = express();
 const recipesController = require('./controllers/recipe');
 
+app.use(cors());
+app.use(helmet());
 app.use('/api/recipes', recipesController);
+
 
 // this will be removed once the endpoints are being created
 // it is just with the initial creation of the express app to test that it is running
