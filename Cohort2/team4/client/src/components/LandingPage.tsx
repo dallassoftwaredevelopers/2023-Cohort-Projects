303c0b4070bea5d15569c20ffcaa343cd26ffa19
ajcodes-cyber <anthonyjohn350@gmail.com>
2023-07-19 22:32:07 -0500

    Add LandingPage to components folder

---
+++ Cohort2/team4/client/src/components/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const LandingPage: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to CONVI</h1>
+      {/* Add more content and design elements as needed */}
+    </div>
+  );
+};
+
+export default LandingPage;
