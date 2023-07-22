1bfed757af86c92dcccc59e42b25f5a5b3f7c5a3
Kitsunebackfire <kurtiveycodes@gmail.com>
2023-07-22 11:15:35 -0500

    add styling and structure to Navbar, add chakra theme, integrate chakra theme into chakra provider

--- Cohort2/team6/client/src/main.tsx
+++ Cohort2/team6/client/src/main.tsx
@@ -4,10 +4,11 @@ import App from "./App.tsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
---
+++ Cohort2/team6/client/src/theme.ts
@@ -0,0 +1,13 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    customScheme: {
+      darkBlue: "#23304A",
+      lightBlue: "#057ADA",
+      // Add more shades if needed
+    },
+  },
+});
+
+export default theme;
