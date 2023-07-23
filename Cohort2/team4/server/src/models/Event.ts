a3507b92f9ae1f2d00cadb87c526620af1a76328
Michelle Song <101286675+em-ess@users.noreply.github.com>
2023-07-22 22:24:33 -0500

    [CONVI-20] tRPC init. Added trpc User management calls. (#46)
    * Added type: module to package,json. Changed module to esnext and added module resolution: node to tsconfig
    
    * Created userRouter calls with test data. Added trpc init to app.ts. Merged routers to appRouter in app.ts.
    
    * Created trpc file. Changed import locations for appRouter.
    
    * Created trpc.ts
    
    * Added timestamps:false to models. Ran npm lint.
    
    * Removed user_id from routers/user.ts. Adjusted variables for calls due to lint issues.

--- Cohort2/team4/server/src/models/Event.ts
+++ Cohort2/team4/server/src/models/Event.ts
@@ -1,51 +1,51 @@
 import { Table, Column, PrimaryKey, Model, AutoIncrement, DataType, AllowNull } from 'sequelize-typescript';
 
-@Table({ tableName: 'event' })
-class Event extends Model {
-    @PrimaryKey
-    @AutoIncrement
-    @Column(DataType.INTEGER)
+@Table({ tableName: 'events', timestamps: false })
+class Events extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
     event_id!: number;
     
-    @AllowNull(true)
-    @Column(DataType.STRING(255))
+  @AllowNull(true)
+  @Column(DataType.STRING(255))
     name!: string | null;
 
-    @AllowNull(true)
-    @Column(DataType.STRING(255))
+  @AllowNull(true)
+  @Column(DataType.STRING(255))
     short_description!: string | null;
 
-    @AllowNull(true)
-    @Column(DataType.BLOB)
+  @AllowNull(true)
+  @Column(DataType.BLOB)
     long_description!: Buffer | null;
 
-    @AllowNull(true)
-    @Column(DataType.STRING(255))
+  @AllowNull(true)
+  @Column(DataType.STRING(255))
     city!: string | null;
 
-    @AllowNull(true)
-    @Column(DataType.STRING(255))
+  @AllowNull(true)
+  @Column(DataType.STRING(255))
     state!: string | null;
 
-    @AllowNull(true)
-    @Column(DataType.STRING(255))
+  @AllowNull(true)
+  @Column(DataType.STRING(255))
     country!: string | null;
 
-    @AllowNull(true)
-    @Column(DataType.STRING(255))
+  @AllowNull(true)
+  @Column(DataType.STRING(255))
     address!: string | null;
 
-    @AllowNull(false)
-    @Column(DataType.DATE)
+  @AllowNull(false)
+  @Column(DataType.DATE)
     start_time!: Date;
 
-    @AllowNull(false)
-    @Column(DataType.DATE)
+  @AllowNull(false)
+  @Column(DataType.DATE)
     end_time!: Date;
     
-    @AllowNull(true)
-    @Column(DataType.INTEGER)
+  @AllowNull(true)
+  @Column(DataType.INTEGER)
     host!: number | null;
 }
 
-export default Event;
+export default Events;
--- Cohort2/team4/server/src/models/User.ts
+++ Cohort2/team4/server/src/models/User.ts
@@ -1,29 +1,29 @@
 import { Table, Column, PrimaryKey, Model, Unique, AutoIncrement, DataType, AllowNull } from 'sequelize-typescript';
 
-@Table({ tableName: 'user' })
-class User extends Model {
-    @PrimaryKey
-    @AutoIncrement
-    @Column(DataType.INTEGER)
+@Table({ tableName: 'users', timestamps: false })
+class Users extends Model {
+  @PrimaryKey
+  @AutoIncrement
+  @Column(DataType.INTEGER)
     user_id!: number;
     
-    @AllowNull(false)
-    @Unique
-    @Column(DataType.STRING(30))
+  @AllowNull(false)
+  @Unique
+  @Column(DataType.STRING(30))
     email!: string;
 
-    @AllowNull(false)
-    @Column(DataType.STRING(30))
+  @AllowNull(false)
+  @Column(DataType.STRING(30))
     first_name!: string;
 
-    @AllowNull(false)
-    @Column(DataType.STRING(30))
+  @AllowNull(false)
+  @Column(DataType.STRING(30))
     last_name!: string;
 
-    @AllowNull(false)
-    @Column(DataType.STRING(255))
+  @AllowNull(false)
+  @Column(DataType.STRING(255))
     auth_key!: string;
     
 }
 
-export default User;
+export default Users;
