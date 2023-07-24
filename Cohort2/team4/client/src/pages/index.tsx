5e7caf8f0b6236e5ae746935cca73e1086b9ae57
ajcodes-cyber <anthonyjohn350@gmail.com>
2023-07-23 20:38:38 -0500

    adds dashboard, profile, and login for index.tsx

--- Cohort2/team4/client/src/pages/index.tsx
+++ Cohort2/team4/client/src/pages/index.tsx
@@ -5,10 +5,29 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 			<div>
-        <img src="/src/images/convi-logo-1.png" alt="Convi Logo"/>
+        <Image 
+          src="/team4/client/src/images/convi-logo-1.png"
+          alt="Convi Logo"
+          width={100}
+          height={100}
+        />
 				<h1>CONVI</h1>
         <p>Your Gateway to Unforgettable Events!</p>
 				<Link href="events">Events Dashboard</Link>
+
+        <p>
+          <Link href="/dashboard">
+            <a>Dashboard</a>
+          </Link>
+          
+          <Link href="/profile">
+            <a>Profile</a>
+          </Link>
+          
+          <Link href="/login">
+            <a>Login</a>
+          </Link>
+</p>
 			</div>
     </main>
   )
